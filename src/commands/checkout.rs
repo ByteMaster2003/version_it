8f60e385dadd64e3b5f7994c1163a75d4fcc14d5
Vivek-Sahani <viveksahani39266@gmail.com>
2025-05-03 21:21:44 +0530

    refactor: Update index entry while checking out other branch

--- src/commands/checkout.rs
+++ src/commands/checkout.rs
@@ -1,5 +1,6 @@
 use crate::utils::{
-    clear_current_tree, decompress_file_content, parse_tree_entries, read_commit_file,
+    FileStatus, IndexEntry, clear_current_tree, decompress_file_content, parse_tree_entries,
+    read_commit_file, read_index, write_index,
 };
 use clap::{Arg, Command};
 use colored::Colorize;
@@ -16,7 +17,8 @@ pub fn get_checkout_command() -> Command {
 }
 
 pub fn checkout(name: &str) {
-    let vit_dir = env::current_dir().unwrap().join(".vit");
+    let current_dir = env::current_dir().unwrap();
+    let vit_dir = current_dir.join(".vit");
     if !vit_dir.exists() {
         return println!("Vit not initialized");
     }
@@ -25,6 +27,8 @@ pub fn checkout(name: &str) {
     let head_path = vit_dir.join("HEAD");
     let head_ref = fs::read_to_string(&head_path).unwrap();
     let current_branch_ref = head_ref.trim_start_matches("ref: ").trim();
+    let mut index_entries: Vec<IndexEntry> = read_index().unwrap();
+    let mut files_to_keep: Vec<String> = Vec::new();
 
     if Path::new(vit_dir.join(current_branch_ref).as_path())
         .file_name()
@@ -59,17 +63,40 @@ pub fn checkout(name: &str) {
 
     let base_path = env::current_dir().unwrap();
     clear_current_tree(&base_path);
-    restore_tree(&tree_path, &base_path, &vit_dir.join("objects"));
+    restore_tree(
+        &tree_path,
+        &base_path,
+        &vit_dir.join("objects"),
+        &mut index_entries,
+        &mut files_to_keep,
+    );
 
+    files_to_keep = files_to_keep
+        .iter()
+        .map(|file| file.replace(current_dir.to_str().unwrap(), "."))
+        .collect();
+
+    index_entries.retain(|entry| files_to_keep.contains(&entry.path));
+    write_index(&index_entries, vit_dir.join("index").to_str().unwrap()).unwrap();
     println!("Checkout to branch --> {}", &name);
 }
 
-fn restore_tree(tree_path: &Path, base_path: &Path, objects_path: &Path) {
+fn restore_tree(
+    tree_path: &Path,
+    base_path: &Path,
+    objects_path: &Path,
+    index_entries: &mut Vec<IndexEntry>,
+    files_to_keep: &mut Vec<String>,
+) {
     let tree_entries = parse_tree_entries(&tree_path).unwrap();
 
     for entry in tree_entries {
         let mode = entry.mode;
         let name = base_path.join(&entry.name);
+        let file_path = name
+            .to_string_lossy()
+            .to_string()
+            .replace(env::current_dir().unwrap().to_str().unwrap(), ".");
         let hash = &entry.sha256;
         let hash_str = hex::encode(hash);
 
@@ -79,13 +106,33 @@ fn restore_tree(tree_path: &Path, base_path: &Path, objects_path: &Path) {
             let sub_tree_path = objects_path.join(&hash_str[..2]).join(&hash_str[2..]);
             let sub_base_path = base_path.join(&name);
 
-            restore_tree(&sub_tree_path, &sub_base_path, objects_path);
+            restore_tree(
+                &sub_tree_path,
+                &sub_base_path,
+                objects_path,
+                index_entries,
+                files_to_keep,
+            );
         } else {
             let blob_path = objects_path.join(&hash_str[..2]).join(&hash_str[2..]);
             let decompressed = decompress_file_content(&blob_path).unwrap();
             let blob_data = &decompressed[decompressed.iter().position(|&b| b == 0).unwrap() + 1..];
 
+            // Write file data
             fs::write(&name, blob_data).unwrap();
+
+            // Update the index entry
+            if let Some(i_entry) = index_entries.iter_mut().find(|i| i.path == file_path) {
+                i_entry.sha256 = entry.sha256;
+                i_entry.status = FileStatus::Unchanged;
+            } else {
+                let mut new_entry = IndexEntry::create(&file_path);
+                new_entry.status = FileStatus::Unchanged;
+                index_entries.push(new_entry);
+            }
+
+            // Push into index list
+            files_to_keep.push(file_path);
         }
     }
 }
